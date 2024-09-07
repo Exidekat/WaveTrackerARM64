5f2a0ca4b3e1b3bcf9eb278057a158650600f635
Haydon Behl <exidekat@gmail.com>
2024-09-06 18:52:07 -0700

    NumberBox.cs, NumberBoxDecimal.cs adaptation

--- Source/UI/NumberBox.cs
+++ Source/UI/NumberBox.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 using System;
-using System.Windows.Forms;
 
 namespace WaveTracker.UI {
-    public class NumberBox : Clickable {
-        private Forms.EnterText dialog;
+    public class NumberBox : UserControl {
+        private Window dialog;
         private bool dialogOpen;
 
-        public SpriteButton bUp;
-        public SpriteButton bDown;
+        public Button bUp;
+        public Button bDown;
         private int boxWidth;
         private string label;
         private int min = int.MinValue;
@@ -22,156 +24,125 @@ namespace WaveTracker.UI {
 
         private int lastValue;
         private int _value;
-        public int Value { get { return _value; } set { _value = Math.Clamp(value, min, max); } }
-
-        public NumberBox(string label, int x, int y, int width, int boxWidth, Element parent) {
-            this.label = label;
-            DisplayMode = NumberDisplayMode.Number;
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.boxWidth = boxWidth;
-            height = 13;
-            canScroll = true;
-            SetParent(parent);
-            bUp = new SpriteButton(width - 10, 0, 10, 6, 416, 144, this);
-            bDown = new SpriteButton(width - 10, 7, 10, 6, 416, 176, this);
+        public int Value {
+            get { return _value; }
+            set { _value = Math.Clamp(value, min, max); }
         }
 
-        public NumberBox(string label, int x, int y, Element parent) {
+        public NumberBox(string label, int x, int y, int width, int boxWidth) {
             this.label = label;
-            this.x = x;
-            this.y = y;
-            width = Helpers.GetWidthOfText(label) + 46;
             DisplayMode = NumberDisplayMode.Number;
-            boxWidth = 38;
-            height = 13;
+            this.Margin = new Thickness(x, y, 0, 0);
+            this.Width = width;
+            this.boxWidth = boxWidth;
+            Height = 30;
             canScroll = true;
-            SetParent(parent);
-            bUp = new SpriteButton(width - 10, 0, 10, 6, 416, 144, this);
-            bDown = new SpriteButton(width - 10, 7, 10, 6, 416, 176, this);
+
+            bUp = new Button { Content = "▲", Width = 20, Height = 20, Margin = new Thickness(0, 0, 0, 10) };
+            bUp.Click += (s, e) => Value++;
+
+            bDown = new Button { Content = "▼", Width = 20, Height = 20 };
+            bDown.Click += (s, e) => Value--;
+
+            var panel = new StackPanel { Orientation = Orientation.Horizontal };
+            panel.Children.Add(bUp);
+            panel.Children.Add(bDown);
+
+            this.Content = panel;
+        }
+
+        public void SetValueLimits(int min, int max) {
+            this.min = min;
+            this.max = max;
+            if (Value < min) Value = min;
+            if (Value > max) Value = max;
         }
 
         public void EnableScrolling() { canScroll = true; }
         public void DisableScrolling() { canScroll = false; }
 
-        public void SetValueLimits(int min, int max) {
-            this.min = min;
-            this.max = max;
-            ArgumentOutOfRangeException.ThrowIfLessThan(max, min);
-            if (Value < min) {
-                Value = min;
-            }
-
-            if (Value > max) {
-                Value = max;
-            }
-        }
-
         public void Update() {
-            bUp.enabled = enabled && Value < max;
-            bDown.enabled = enabled && Value > min;
-            if (enabled && InFocus) {
-                int valueBeforeUpdate = Value;
-                if (DoubleClicked && MouseX < width - 10) {
-                    if (!dialogOpen) {
-                        dialogOpen = true;
-                        StartDialog();
-                    }
-                }
-                else {
-                    dialogOpen = false;
-                }
-                if (IsInHierarchy(Input.lastClickFocus)) {
-                    if (LastClickPos.X >= 0 && LastClickPos.Y >= 0) {
-                        if (LastClickPos.X <= width - 10 && LastClickPos.Y <= height) {
-                            if (Input.GetClickDown(KeyModifier.None)) {
-                                valueSaved = Value;
-                            }
+            bUp.IsEnabled = Value < max;
+            bDown.IsEnabled = Value > min;
 
-                            if (Input.GetClick(KeyModifier.None)) {
-                                Value = valueSaved - (MouseY - LastClickPos.Y) / 2;
-                                App.MouseCursorArrow = 2;
-                            }
-                        }
-                    }
-                }
-                if (IsHovered && canScroll) {
-                    Value += Input.MouseScrollWheel(KeyModifier.None);
-                }
-
-                if (bUp.Clicked) {
-                    Value++;
-                }
-
-                if (bDown.Clicked) {
-                    Value--;
-                }
-
-                if (Value != lastValue) {
-                    ValueWasChanged = true;
-                    lastValue = Value;
-                }
-                else {
-                    ValueWasChanged = false;
-                }
-
-                ValueWasChangedInternally = Value != valueBeforeUpdate;
-            }
-        }
-
-        public void Draw() {
-            Color dark = UIColors.label;
-            Color text = UIColors.black;
-            Color labelCol = UIColors.labelDark;
-            if (IsHovered && enabled) {
-                labelCol = UIColors.black;
-                dark = UIColors.label;
-            }
-            int bWidth = boxWidth - 10;
-            int boxStart = width - boxWidth;
-            int boxHeight = 13;
-            int boxStartY = (height - boxHeight) / 2;
-            Write(label + "", 0, height / 2 - 3, labelCol);
-            DrawRect(boxStart, boxStartY, bWidth, boxHeight, dark);
-            DrawRect(boxStart + 1, boxStartY + 1, bWidth - 2, boxHeight - 2, Color.White);
-            DrawRect(boxStart + 1, boxStartY + 1, bWidth - 2, 1, new Color(193, 196, 213));
-            DrawRect(width, boxStartY + 6, -10, 1, ButtonColors.Round.backgroundColor);
-            switch (DisplayMode) {
-                case NumberDisplayMode.Number:
-                    Write(Value + "", boxStart + 4, height / 2 - 3, text);
-                    break;
-                case NumberDisplayMode.Note:
-                    Write(Value + " (" + Helpers.MIDINoteToText(Value) + ")", boxStart + 4, height / 2 - 3, text);
-                    break;
-                case NumberDisplayMode.NoteOnly:
-                    Write(Helpers.MIDINoteToText(Value), boxStart + 4, height / 2 - 3, text);
-                    break;
-                case NumberDisplayMode.PlusMinus:
-                    Write((Value <= 0 ? Value : "+" + Value) + "", boxStart + 4, height / 2 - 3, text);
-                    break;
-                case NumberDisplayMode.Percent:
-                    Write(Value + "%", boxStart + 4, height / 2 - 3, text);
-                    break;
-                case NumberDisplayMode.Milliseconds:
-                    Write(Value + "ms", boxStart + 4, height / 2 - 3, text);
-                    break;
+            if (dialogOpen && !IsMouseOver) {
+                dialogOpen = false;
             }
 
-            bUp.Draw();
-            bDown.Draw();
+            if (IsPointerOver) {
+                if (canScroll) {
+                    this.AddHandler(PointerWheelChangedEvent, (s, e) => {
+                        Value += (int)e.Delta.Y; // Scroll to adjust value
+                        e.Handled = true;
+                    });
+                }
+            }
+
+            ValueWasChangedInternally = Value != lastValue;
+            lastValue = Value;
         }
 
         public void StartDialog() {
-            Input.DialogStarted();
-            dialog = new Forms.EnterText();
-            dialog.textBox.Text = Value + "";
-            dialog.label.Text = label;
-            if (dialog.ShowDialog() == DialogResult.OK) {
-                if (int.TryParse(dialog.textBox.Text, out int a)) {
-                    Value = a;
-                }
+            if (!dialogOpen) {
+                dialogOpen = true;
+
+                dialog = new Window {
+                    Width = 300,
+                    Height = 150,
+                    Title = "Enter Value"
+                };
+
+                var textBox = new TextBox { Text = Value.ToString(), Margin = new Thickness(10) };
+                var okButton = new Button { Content = "OK", Width = 100, Margin = new Thickness(10) };
+                okButton.Click += (s, e) => {
+                    if (int.TryParse(textBox.Text, out int result)) {
+                        Value = result;
+                    }
+                    dialog.Close();
+                };
+
+                var stackPanel = new StackPanel { Orientation = Orientation.Vertical };
+                stackPanel.Children.Add(textBox);
+                stackPanel.Children.Add(okButton);
+                dialog.Content = stackPanel;
+
+                dialog.ShowDialog(App.MainWindow);
             }
+        }
+
+        public override void Render(DrawingContext context) {
+            base.Render(context);
+            var rectBrush = new SolidColorBrush(Colors.White);
+            var borderBrush = new SolidColorBrush(Colors.Gray);
+            var textBrush = new SolidColorBrush(Colors.Black);
+
+            var boxRect = new Rect(Bounds.Width - boxWidth, 0, boxWidth - 20, Bounds.Height);
+
+            // Draw Box
+            context.DrawRectangle(rectBrush, new Pen(borderBrush, 1), boxRect);
+
+            // Draw Text
+            string textToDisplay = DisplayMode switch {
+                NumberDisplayMode.Note => $"{Value} ({Helpers.MIDINoteToText(Value)})",
+                NumberDisplayMode.NoteOnly => Helpers.MIDINoteToText(Value),
+                NumberDisplayMode.PlusMinus => Value <= 0 ? Value.ToString() : $"+{Value}",
+                NumberDisplayMode.Percent => $"{Value}%",
+                NumberDisplayMode.Milliseconds => $"{Value}ms",
+                _ => Value.ToString()
+            };
+            var formattedText = new FormattedText {
+                Text = textToDisplay,
+                Typeface = new Typeface("Arial"),
+                FontSize = 12,
+                TextAlignment = TextAlignment.Left
+            };
+
+            var textPoint = new Point(boxRect.Left + 5, (Bounds.Height - formattedText.Bounds.Height) / 2);
+            context.DrawText(textBrush, textPoint, formattedText);
+
+            // Draw buttons
+            bUp.Render(context);
+            bDown.Render(context);
         }
     }
 }
--- Source/UI/NumberBoxDecimal.cs
+++ Source/UI/NumberBoxDecimal.cs
@@ -1,22 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 using System;
-using System.Windows.Forms;
 
 namespace WaveTracker.UI {
-    public class NumberBoxDecimal : Clickable {
-        private Forms.EnterText dialog;
+    public class NumberBoxDecimal : UserControl {
+        private Window dialog;
         private bool dialogOpen;
 
-        public SpriteButton bUp;
-        public SpriteButton bDown;
+        public Button bUp;
+        public Button bDown;
         private int boxWidth;
         private string label;
         private float min = float.MinValue;
         private float max = float.MaxValue;
         private int valueSaved;
         private bool canScroll = true;
+
         public enum DisplayMode { Number, Note, NoteOnly, PlusMinus }
         public DisplayMode displayMode = DisplayMode.Number;
+
         public bool ValueWasChanged { get; private set; }
         public bool ValueWasChangedInternally { get; private set; }
 
@@ -24,128 +28,121 @@ namespace WaveTracker.UI {
         private int _value;
 
         private int DecimalPlaces { get; set; }
-        public float Value { get { return _value / powersOfTen[DecimalPlaces]; } set { _value = (int)Math.Clamp(value * powersOfTen[DecimalPlaces], min * powersOfTen[DecimalPlaces], max * powersOfTen[DecimalPlaces]); } }
+
+        public float Value {
+            get { return _value / powersOfTen[DecimalPlaces]; }
+            set { _value = (int)Math.Clamp(value * powersOfTen[DecimalPlaces], min * powersOfTen[DecimalPlaces], max * powersOfTen[DecimalPlaces]); }
+        }
 
         private static int[] powersOfTen = { 1, 10, 100, 1000, 10000, 100000 };
 
-        public NumberBoxDecimal(string label, int x, int y, int width, int boxWidth, Element parent, int decimalPlaces = 2) {
+        public NumberBoxDecimal(string label, int x, int y, int width, int boxWidth, int decimalPlaces = 2) {
             this.label = label;
-            this.x = x;
-            this.y = y;
-            this.width = width;
+            this.Margin = new Thickness(x, y, 0, 0);
+            this.Width = width;
             this.boxWidth = boxWidth;
             DecimalPlaces = decimalPlaces;
-            height = 13;
+            Height = 30;
             canScroll = true;
-            SetParent(parent);
-            bUp = new SpriteButton(width - 10, 0, 10, 6, 416, 144, this);
-            bDown = new SpriteButton(width - 10, 7, 10, 6, 416, 176, this);
+
+            bUp = new Button { Content = "▲", Width = 20, Height = 20, Margin = new Thickness(0, 0, 0, 10) };
+            bUp.Click += (s, e) => Value++;
+
+            bDown = new Button { Content = "▼", Width = 20, Height = 20 };
+            bDown.Click += (s, e) => Value--;
+
+            var panel = new StackPanel { Orientation = Orientation.Horizontal };
+            panel.Children.Add(bUp);
+            panel.Children.Add(bDown);
+
+            this.Content = panel;
         }
 
-        public NumberBoxDecimal(string label, int x, int y, Element parent, int decimalPlaces = 2) {
-            this.label = label;
-            this.x = x;
-            this.y = y;
-            width = Helpers.GetWidthOfText(label) + 46;
-            boxWidth = 38;
-            DecimalPlaces = decimalPlaces;
-            height = 13;
-            canScroll = true;
-            SetParent(parent);
-            bUp = new SpriteButton(width - 10, 0, 10, 6, 416, 144, this);
-            bDown = new SpriteButton(width - 10, 7, 10, 6, 416, 176, this);
+        public void SetValueLimits(float min, float max) {
+            this.min = min;
+            this.max = max;
+            if (Value < min) Value = min;
+            if (Value > max) Value = max;
         }
 
         public void EnableScrolling() { canScroll = true; }
         public void DisableScrolling() { canScroll = false; }
 
-        public void SetValueLimits(int min, int max) {
-            this.min = min;
-            this.max = max;
-        }
-
         public void Update() {
-            if (enabled && InFocus) {
-                int valueBeforeUpdate = _value;
-                if (DoubleClicked && MouseX < width - 10) {
-                    if (!dialogOpen) {
-                        dialogOpen = true;
-                        StartDialog();
-                    }
-                }
-                else {
-                    dialogOpen = false;
-                }
-                if (LastClickPos.X >= 0 && LastClickPos.Y >= 0) {
-                    if (LastClickPos.X <= width - 10 && LastClickPos.Y <= height) {
-                        if (Input.GetClickDown(KeyModifier.None)) {
-                            valueSaved = _value;
-                        }
+            bUp.IsEnabled = Value < max;
+            bDown.IsEnabled = Value > min;
 
-                        if (Input.GetClick(KeyModifier.None)) {
-                            _value = valueSaved - (MouseY - LastClickPos.Y) / 2;
-                            App.MouseCursorArrow = 2;
-                        }
-                    }
-                }
-                bUp.enabled = Value < max;
-                bDown.enabled = Value > min;
-                if (IsHovered && canScroll) {
-                    Value += Input.MouseScrollWheel(KeyModifier.None);
-                }
-
-                if (bUp.Clicked) {
-                    Value++;
-                }
-
-                if (bDown.Clicked) {
-                    Value--;
-                }
-
-                if (_value != lastValue) {
-                    ValueWasChanged = true;
-                    lastValue = _value;
-                }
-                else {
-                    ValueWasChanged = false;
-                }
-
-                ValueWasChangedInternally = _value != valueBeforeUpdate;
+            if (dialogOpen && !IsMouseOver) {
+                dialogOpen = false;
             }
-        }
 
-        public void Draw() {
-            Color dark = UIColors.label;
-            Color text = UIColors.black;
-            Color labelCol = UIColors.labelDark;
-            if (IsHovered) {
-                labelCol = Color.Black;
-                dark = UIColors.label;
+            if (IsPointerOver) {
+                if (canScroll) {
+                    this.AddHandler(PointerWheelChangedEvent, (s, e) => {
+                        Value += (float)e.Delta.Y / powersOfTen[DecimalPlaces]; // Scroll to adjust value
+                        e.Handled = true;
+                    });
+                }
             }
-            int bWidth = boxWidth - 10;
-            int boxStart = width - boxWidth;
-            int boxHeight = 13;
-            int boxStartY = (height - boxHeight) / 2;
-            Write(label + "", 0, height / 2 - 3, labelCol);
-            DrawRect(boxStart, boxStartY, bWidth, boxHeight, dark);
-            DrawRect(boxStart + 1, boxStartY + 1, bWidth - 2, boxHeight - 2, Color.White);
-            DrawRect(boxStart + 1, boxStartY + 1, bWidth - 2, 1, new Color(193, 196, 213));
-            DrawRect(width, boxStartY + 6, -10, 1, ButtonColors.Round.backgroundColor);
-            Write(Value.ToString("D" + DecimalPlaces), boxStart + 4, height / 2 - 3, text);
-            bUp.Draw();
-            bDown.Draw();
+
+            ValueWasChangedInternally = _value != lastValue;
+            lastValue = _value;
         }
 
         public void StartDialog() {
-            Input.DialogStarted();
-            dialog = new Forms.EnterText();
-            dialog.textBox.Text = Value + "";
-            dialog.label.Text = label;
-            if (dialog.ShowDialog() == DialogResult.OK) {
-                if (float.TryParse(dialog.textBox.Text, out float a)) {
-                    Value = a;
-                }
+            if (!dialogOpen) {
+                dialogOpen = true;
+
+                dialog = new Window {
+                    Width = 300,
+                    Height = 150,
+                    Title = "Enter Value"
+                };
+
+                var textBox = new TextBox { Text = Value.ToString(), Margin = new Thickness(10) };
+                var okButton = new Button { Content = "OK", Width = 100, Margin = new Thickness(10) };
+                okButton.Click += (s, e) => {
+                    if (float.TryParse(textBox.Text, out float result)) {
+                        Value = result;
+                    }
+                    dialog.Close();
+                };
+
+                var stackPanel = new StackPanel { Orientation = Orientation.Vertical };
+                stackPanel.Children.Add(textBox);
+                stackPanel.Children.Add(okButton);
+                dialog.Content = stackPanel;
+
+                dialog.ShowDialog(App.MainWindow);
             }
+        }
+
+        public override void Render(DrawingContext context) {
+            base.Render(context);
+            var rectBrush = new SolidColorBrush(Colors.White);
+            var borderBrush = new SolidColorBrush(Colors.Gray);
+            var textBrush = new SolidColorBrush(Colors.Black);
+
+            var boxRect = new Rect(Bounds.Width - boxWidth, 0, boxWidth - 20, Bounds.Height);
+
+            // Draw Box
+            context.DrawRectangle(rectBrush, new Pen(borderBrush, 1), boxRect);
+
+            // Draw Text
+            string textToDisplay = Value.ToString($"F{DecimalPlaces}");
+            var formattedText = new FormattedText {
+                Text = textToDisplay,
+                Typeface = new Typeface("Arial"),
+                FontSize = 12,
+                TextAlignment = TextAlignment.Left
+            };
+
+            var textPoint = new Point(boxRect.Left + 5, (Bounds.Height - formattedText.Bounds.Height) / 2);
+            context.DrawText(textBrush, textPoint, formattedText);
+
+            // Draw buttons
+            bUp.Render(context);
+            bDown.Render(context);
         }
     }
 }
